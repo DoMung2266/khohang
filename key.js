a3c78b7a1d582a7ab09f4846033673e081262b4e
DoMung2266 <nhamayvanmung@gmail.com>
2025-07-30 10:59:22 +0700

    Create key.js

---
+++ key.js
@@ -0,0 +1,8 @@
+//key.js
+// File cấu hình OAuth 
+const GOOGLE_CONFIG = {
+  client_id: "875883725470-la5ao5l07bat3epei59g3uirgqenedds.apps.googleusercontent.com"
+};
+
+// Token được gán sau khi đăng nhập
+let accessToken = '';
